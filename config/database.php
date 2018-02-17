e4e9742c18fae2e2f70077f4a73a2aea736e065e
Sai Abhinav <saiabhinavreddyvasepalli@gmail.com>
2018-02-17 23:25:36 +0530

    changed db again

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'db4free.net'),
+            'host' => env('DB_HOST', 'sql12.freemysqlhosting.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'appetite'),
-            'username' => env('DB_USERNAME', 'appetite'),
-            'password' => env('DB_PASSWORD', 'rootroot'),
+            'database' => env('DB_DATABASE', 'sql12221806'),
+            'username' => env('DB_USERNAME', 'sql12221806'),
+            'password' => env('DB_PASSWORD', 'JWytMQss9p'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
